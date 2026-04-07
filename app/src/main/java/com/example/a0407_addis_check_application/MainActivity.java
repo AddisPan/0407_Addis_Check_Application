5fbf5925b55429fde533dce26a742b1a161cd480
addispan <addispan@moneydj.com>
2026-04-07 16:46:22 +0800

    // 上面標籤列 中間標題 右邊圖片 16:42 完成 16:46

--- app/src/main/java/com/example/a0407_addis_check_application/MainActivity.java
+++ app/src/main/java/com/example/a0407_addis_check_application/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 上面標籤列 中間標題 右邊圖片 16:42 完成 16:46
     }
 }
