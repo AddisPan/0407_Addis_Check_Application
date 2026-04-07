655471925d26f9b41cd1697e009823d64829868c
addispan <addispan@moneydj.com>
2026-04-07 17:39:18 +0800

    // 製作抽象工廠顯示在Activity上 17:33 完成 17:39

--- app/src/main/java/com/example/a0407_addis_check_application/MainActivity.java
+++ app/src/main/java/com/example/a0407_addis_check_application/MainActivity.java
@@ -15,16 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 製作抽象工廠(Log能成功顯示) 顯示 17:25 完成 17:32
+        // 製作抽象工廠顯示在Activity上 17:33 完成 17:39
         TextView mTextViewCorn = findViewById(R.id.id_textview_corn);
         TextView mTextViewFish = findViewById(R.id.id_textview_fish);
         TextView mTextViewShow = findViewById(R.id.id_textview_show);
 
+        final String[] s = {"", ""};
         Factory.CornFactory mCornFactory = new Factory.CornFactory();
         mCornFactory.createFood().Type();
         mCornFactory.createContainer().Type();
+        mTextViewCorn.setOnClickListener(v->{
+            s[0] = mCornFactory.createFood().Type();
+            s[1] = mCornFactory.createContainer().Type();
+            mTextViewShow.setText(s[0]+"\n"+s[1]);
+        });
         Factory.FishFactory mFishFactory = new Factory.FishFactory();
         mFishFactory.createFood().Type();
         mFishFactory.createContainer().Type();
+        mTextViewFish.setOnClickListener(v->{
+            s[0] = mFishFactory.createFood().Type();
+            s[1] = mFishFactory.createContainer().Type();
+            mTextViewShow.setText(s[0]+"\n"+s[1]);
+        });
     }
 }
