b94dd604f1ac87ec9dc41beeb68f2da9bf542737
addispan <addispan@moneydj.com>
2026-04-16 14:35:18 +0800

    // 畫面上有一個付款明細區塊，用來顯示付款方式及消費金額，顯示再Activity上(點選不同得付款方式會有金額顯示) 開始 14:24  結束 14:35

--- app/src/main/java/com/example/a0407_addis_check_application/MainActivity.java
+++ app/src/main/java/com/example/a0407_addis_check_application/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a0407_addis_check_application;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 畫面上有一個獲得商品區塊，用來顯示付款後獲得的商品(抽象工廠模式生產產品) 開始 14:15  結束 14:23
+        // 畫面上有一個付款明細區塊，用來顯示付款方式及消費金額，顯示再Activity上(點選不同得付款方式會有金額顯示) 開始 14:24  結束 14:35
+        TextView mTextViewLinePay = findViewById(R.id.id_button_line_pay);
+        TextView mTextViewApplePay = findViewById(R.id.id_button_apple_pay);
+        TextView mTextViewShowPrice = findViewById(R.id.id_show_price);
+
         double price = 100;
-        double ApplePayPrice = price*.8;
+        double ApplePayPrice = price * .8;
+
         ChoosePay mChoosePay = new ChoosePay();
-        mChoosePay.choosePay(new PayStrategy.LinePay());
-        mChoosePay.PayName_Price(price);
-        mChoosePay.choosePay(new PayStrategy.ApplePay());
-        mChoosePay.PayName_Price(ApplePayPrice);
+        mTextViewLinePay.setOnClickListener(v -> {
+            mChoosePay.choosePay(new PayStrategy.LinePay());
+            mTextViewShowPrice.setText("" + mChoosePay.PayName_Price(price));
+        });
+        mTextViewApplePay.setOnClickListener(v -> {
+            mChoosePay.choosePay(new PayStrategy.ApplePay());
+            mTextViewShowPrice.setText("" + mChoosePay.PayName_Price(ApplePayPrice));
+        });
     }
 }
